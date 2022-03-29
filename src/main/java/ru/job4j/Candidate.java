7ffcd43a18e5d6d3eca673b5cdfb14209bd41812
Olga_ <olga_art1025@mail.ru>
2022-03-30 00:22:11 +0500

    проект "Работа мечты" модели данныx

---
+++ src/main/java/ru/job4j/Candidate.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Candidate {
+    private final int id;
+    private final String name;
+    private final String resume;
+
+    public Candidate(int id, String name, String resume) {
+        this.id = id;
+        this.name = name;
+        this.resume = resume;
+    }
+}
---
+++ src/main/java/ru/job4j/HR.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class HR {
+    private final int id;
+    private final String name;
+    private final String vacancy;
+
+    public HR(int id, String name, String vacancy) {
+        this.id = id;
+        this.name = name;
+        this.vacancy = vacancy;
+    }
+}
