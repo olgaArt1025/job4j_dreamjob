d72a87a4b1618cafcb147c6eeec0850e0b52e76a
Olga_ <olga_art1025@mail.ru>
2022-04-03 19:46:40 +0500

    3. Редактирование вакансии. [#277566]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -41,11 +41,18 @@ public class Store {
     }
 
     public void save(Post post) {
-        post.setId(POST_ID.incrementAndGet());
+        if (post.getId() == 0) {
+            post.setId(POST_ID.incrementAndGet());
+        }
         posts.put(post.getId(), post);
     }
+
     public void saveCandidate(Candidate candidate) {
         candidate.setId(CANDIDATE_ID.incrementAndGet());
         candidates.put(candidate.getId(), candidate);
+    }
+
+    public Post findById(int id) {
+        return posts.get(id);
     }
 }
