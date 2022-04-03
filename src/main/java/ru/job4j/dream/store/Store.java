5a0a51ea0822decd6a59dcf196ccd25f45767a8c
Olga_ <olga_art1025@mail.ru>
2022-04-03 19:09:25 +0500

    2. CandidateSerlvet. Создание кандидата. [#2513]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -17,6 +17,8 @@ public class Store {
 
     private static AtomicInteger POST_ID = new AtomicInteger(4);
 
+    private static AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
+
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
         posts.put(2, new Post(2, "Middle Java Job"));
@@ -41,5 +43,9 @@ public class Store {
     public void save(Post post) {
         post.setId(POST_ID.incrementAndGet());
         posts.put(post.getId(), post);
+    }
+    public void saveCandidate(Candidate candidate) {
+        candidate.setId(CANDIDATE_ID.incrementAndGet());
+        candidates.put(candidate.getId(), candidate);
     }
 }
